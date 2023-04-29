88a8813d55d18a6117708cdd507486eea5ad75fd
Varneon <me@varneon.com>
2023-04-29 05:57:33 -0700

    Add field attributes

---
+++ Packages/com.varneon.vudon.editors/Runtime/FieldLabelAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Varneon.VUdon.Editors
+{
+    /// <summary>
+    /// Add this attribute to set a custom label for a field
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class FieldLabelAttribute : Attribute
+    {
+        /// <summary>
+        /// The custom field name
+        /// </summary>
+        public string FieldName;
+
+        public FieldLabelAttribute(string fieldName)
+        {
+            FieldName = fieldName;
+        }
+    }
+}
---
+++ Packages/com.varneon.vudon.editors/Runtime/FoldoutHeaderAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Varneon.VUdon.Editors
+{
+    /// <summary>
+    /// Add this attribute to a field to draw it under a foldout
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class FoldoutHeaderAttribute : Attribute
+    {
+        /// <summary>
+        /// The header text
+        /// </summary>
+        public string Header { get; }
+
+        public FoldoutHeaderAttribute(string header)
+        {
+            Header = header;
+        }
+    }
+}
