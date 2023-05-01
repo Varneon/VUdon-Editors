4951d6d877a118e1b1e7cc2c2e313ac43c431e98
Varneon <me@varneon.com>
2023-04-30 23:14:21 -0700

    Add field null warning attribute

---
+++ Packages/com.varneon.vudon.editors/Runtime/FieldNullWarningAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Varneon.VUdon.Editors
+{
+    /// <summary>
+    /// Add this attribute to add a warning if an object field's reference is null
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class FieldNullWarningAttribute : MultiPropertyAttribute
+    {
+#if !COMPILER_UDONSHARP
+        public override FieldAttributeType Type => FieldAttributeType.NullWarning;
+#endif
+
+        /// <summary>
+        /// Should the severity of null state be error
+        /// </summary>
+        public readonly bool IsError;
+
+        /// <summary>
+        /// Add this attribute to add a warning if an object field's reference is null
+        /// </summary>
+        /// <param name="isError">Should the severity of null state be error</param>
+        public FieldNullWarningAttribute(bool isError = false)
+        {
+            IsError = isError;
+
+            this.order = 1;
+        }
+    }
+}
