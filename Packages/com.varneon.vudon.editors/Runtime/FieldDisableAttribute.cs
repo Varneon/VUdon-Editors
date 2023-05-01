449d525875a9a09f151d2adbb0f57c21b7b24476
Varneon <me@varneon.com>
2023-04-30 23:14:05 -0700

    Add field disable attribute

---
+++ Packages/com.varneon.vudon.editors/Runtime/FieldDisableAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Varneon.VUdon.Editors
+{
+    /// <summary>
+    /// Add this attribute to disable a field based on boolean field's state
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class FieldDisableAttribute : MultiPropertyAttribute
+    {
+#if !COMPILER_UDONSHARP
+        public override FieldAttributeType Type => FieldAttributeType.Disable;
+#endif
+
+        /// <summary>
+        /// Name of the boolean field
+        /// </summary>
+        public readonly string Property;
+
+        /// <summary>
+        /// Should the field be disabled when the value is true
+        /// </summary>
+        public readonly bool WhenTrue;
+
+        /// <summary>
+        /// Add this attribute to disable a field based on boolean property's state
+        /// </summary>
+        /// <param name="property">Name of the boolean field</param>
+        /// <param name="whenTrue">Should the field be disabled when the value is true</param>
+        public FieldDisableAttribute(string property, bool whenTrue = false)
+        {
+            Property = property;
+
+            WhenTrue = whenTrue;
+
+            this.order = 1;
+        }
+    }
+}
