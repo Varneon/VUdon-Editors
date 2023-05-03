b7518733d278997e7eb094cfcdf3fc38b6e18615
Varneon <me@varneon.com>
2023-05-03 00:38:37 -0700

    Add LogicType enum

---
+++ Packages/com.varneon.vudon.editors/Runtime/LogicType.cs
@@ -0,0 +1,30 @@
+﻿namespace Varneon.VUdon.Editors
+{
+    public enum LogicType
+    {
+        /// <summary>
+        /// All are true
+        /// </summary>
+        AND,
+        /// <summary>
+        /// Either is true
+        /// </summary>
+        OR,
+        /// <summary>
+        /// Either is false
+        /// </summary>
+        NAND,
+        /// <summary>
+        /// All are false
+        /// </summary>
+        NOR,
+        /// <summary>
+        /// Only one is true
+        /// </summary>
+        XOR,
+        /// <summary>
+        /// All are equal
+        /// </summary>
+        XNOR
+    }
+}
